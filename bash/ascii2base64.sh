1246287434232c5e952fb1277be9cb7d8547a71d
AssassinUKG <more4money@gmail.com>
2021-02-10 01:35:24 +0000

    Update ascii2base64.sh

--- bash/ascii2base64.sh
+++ bash/ascii2base64.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 #Created by Ac1d
-
-if [ ${#@} -lt 1 ]; then
-    banner
-    printf "Give a ASCII ART file ...,Exiting..."
-    exit;
-fi
-
 banner() {
     asciiart=$(base64 -d <<< "H4sIAAAAAAAAA21RwRHDMAj7ewo9+zIbdBJdWcTDF4HtOLlwTiAgZEEAuAwP6+7dT+tPxGULgs3V
 AIsPgLucDsM9DieMmbgEnMgkkFmGQ6cpMqd4q2ymN0cBZouNcCC9BBxI0PKalc8ub4oUIpvUKm0h
@@ -15,14 +8,17 @@ n4L2B8ZvFjwKAgAA" | gunzip)
     echo  "$asciiart"
 }
 
+if [ ${#@} -lt 1 ]; then
+    banner
+    printf "Give a ASCII ART file (./asii2base64 asciifile) ...,Exiting..."
+    exit;
+fi
+
 usage() {
-    echo -e "\nUsage:\n1. save asciii art to file (nano)\n2. ./ascii2base64.sh file\n"
+    echo -e "\nUsage:\n1. save asciii art to file (nano)\n2. ./ascii2base64 file\n"
 }
-
-
 banner
 usage
-
 art1=$(cat "$1" | gzip -cf | base64)
 echo -e "\nCommand:\nasciiart=\$(base64 -d <<< \"$art1\" | gunzip) \n\nOutput:\n"
 asciiDecode=$(base64 -d <<< "$art1" | gunzip)
