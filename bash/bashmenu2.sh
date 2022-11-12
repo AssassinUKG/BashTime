ce6106fa1ca9d1a925b0e5bc54ac2e53f0b918b0
ac1d <more4money@gmail.com>
2022-11-12 20:04:07 +0000

    Create bashmenu2.sh

---
+++ bash/bashmenu2.sh
@@ -0,0 +1,30 @@
+check_update () {
+    echo "Checking update"
+}
+
+reinstall_theme () {
+    echo "Reinstalling theme"
+}
+
+while true; do
+    options=("Check for update" "Reinstall theme")
+
+    echo "Choose an option:"
+    select opt in "${options[@]}"; do
+        case $REPLY in
+            1) check_update; break ;;
+            2) reinstall_theme; break ;;
+            *) echo "What's that?" >&2
+        esac
+    done
+
+    echo "Doing other things..."
+
+    echo "Are we done?"
+    select opt in "Yes" "No"; do
+        case $REPLY in
+            1) break 2 ;;
+            2) break ;;
+            *) echo "Look, it's a simple question..." >&2
+        esac
+    d
